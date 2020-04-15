c7a1c6abcfcc8ea210c52bd091f102aa3856eeb8
Matrai Ba <matrai.barnabas1@gmail.com>
2020-04-15 12:40:26 +0200

    create basic react project

---
+++ online-digit-recognizer/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Paint from './components/paint/paint';
+
+function App() {
+  return (
+    <div className="App">
+        <Paint/>
+    </div>
+  );
+}
+
+export default App;
---
+++ online-digit-recognizer/src/components/paint/paint.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './paint.css';
+
+function Paint() {
+    return (
+        <table className="image">
+            <tr className="table-row" >
+                <th id="0" className="table-header" />
+                <th id="1" className="table-header" />
+                <th id="2" className="table-header" />
+                <th id="3" className="table-header" />
+                <th id="4" className="table-header" />
+            </tr>
+            <tr className="table-row" >
+                <th id="5" className="table-header" />
+                <th id="6" className="table-header" />
+                <th id="7" className="table-header" />
+                <th id="8" className="table-header" />
+                <th id="9" className="table-header" />
+            </tr>
+            <tr className="table-row" >
+                <th id="10" className="table-header" />
+                <th id="11" className="table-header" />
+                <th id="12" className="table-header" />
+                <th id="13" className="table-header" />
+                <th id="14" className="table-header" />
+            </tr>
+        </table>
+    );
+}
+
+export default Paint;
