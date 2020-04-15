d3343d4e71541e05a4de4f2cd3ed05aebcfce6bb
Matrai Ba <matrai.barnabas1@gmail.com>
2020-04-15 13:30:00 +0200

    create basic matrix

--- online-digit-recognizer/src/components/paint/paint.js
+++ online-digit-recognizer/src/components/paint/paint.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './paint.css';
 
-function Paint() {
-    return (
-        <table className="image">
-            <tr className="table-row" >
-                <th id="0" className="table-header" />
-                <th id="1" className="table-header" />
-                <th id="2" className="table-header" />
-                <th id="3" className="table-header" />
-                <th id="4" className="table-header" />
-            </tr>
-            <tr className="table-row" >
-                <th id="5" className="table-header" />
-                <th id="6" className="table-header" />
-                <th id="7" className="table-header" />
-                <th id="8" className="table-header" />
-                <th id="9" className="table-header" />
-            </tr>
-            <tr className="table-row" >
-                <th id="10" className="table-header" />
-                <th id="11" className="table-header" />
-                <th id="12" className="table-header" />
-                <th id="13" className="table-header" />
-                <th id="14" className="table-header" />
-            </tr>
-        </table>
-    );
+class Paint extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        const target = e.target;
+
+        if (target.style.background === "black" || target.style.background === "") {
+            target.style.background = "white";
+        }
+        else {
+            e.target.style.background = "black";
+        }
+    }
+
+    render() {
+        return (
+            <table className="image">
+                <tbody>
+                    <tr className="table-row" >
+                        <th id="0" className="table-header" onClick={this.handleClick} />
+                        <th id="1" className="table-header" onClick={this.handleClick} />
+                        <th id="2" className="table-header" onClick={this.handleClick} />
+                    </tr>
+                    <tr className="table-row" >
+                        <th id="5" className="table-header" onClick={this.handleClick} />
+                        <th id="6" className="table-header" onClick={this.handleClick} />
+                        <th id="7" className="table-header" onClick={this.handleClick} />
+                    </tr>
+                    <tr className="table-row" >
+                        <th id="10" className="table-header" onClick={this.handleClick} />
+                        <th id="11" className="table-header" onClick={this.handleClick} />
+                        <th id="12" className="table-header" onClick={this.handleClick} />
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
 }
 
 export default Paint;
