6fdfc492f44eb3794804a28c5498e86268b8111f
Matrai Ba <matrai.barnabas1@gmail.com>
2020-04-16 13:33:30 +0200

    can now drag mouse to change square color

--- online-digit-recognizer/src/components/paint/paint.js
+++ online-digit-recognizer/src/components/paint/paint.js
@@ -6,16 +6,31 @@ class Paint extends Component {
         super(props);
         this.handleClick = this.handleClick.bind(this);
         this.setupTable = this.setupTable.bind(this);
+        this.switchMouseIsDown = this.switchMouseIsDown.bind(this);
     }
 
     handleClick(e) {
         const target = e.target;
+        const hiddenInput2 = document.getElementById("hidden-input2").value;
 
-        if (target.className === "table-header black") {
-            target.className = "table-header white";
+        if (hiddenInput2 !== target.id && document.getElementById("hidden-input1").value === "true")
+            if (target.className === "table-header black") {
+                target.className = "table-header white";
+            }
+            else {
+                target.className = "table-header black";
+            }
+        document.getElementById("hidden-input2").value = target.id;
+    }
+
+    switchMouseIsDown() {
+        const hiddenInput1 = document.getElementById("hidden-input1").value;
+
+        if (hiddenInput1 === "true") {
+            document.getElementById("hidden-input1").value = "false";
         }
         else {
-            target.className = "table-header black";
+            document.getElementById("hidden-input1").value = "true";
         }
     }
 
@@ -24,7 +39,13 @@ class Paint extends Component {
         const tableSize = Math.pow(tableLength, 2);
 
         for (let i = 0; i < tableSize; i++) {
-            headers.push(React.createElement("th", { key: i, className: "table-header black", id: i, onClick: this.handleClick }));
+            headers.push(React.createElement("th", {
+                key: i,
+                className: "table-header black",
+                id: i,
+                onMouseMove: this.handleClick,
+                onClick: this.switchMouseIsDown
+            }));
         }
 
         const rows = [];
@@ -34,9 +55,14 @@ class Paint extends Component {
         }
 
         const tbody = React.createElement("tbody", { id: "table-body" }, rows);
-        const table = React.createElement("table", { id: "table-main" }, tbody);
+        const table = React.createElement("table", { key: "table-main", id: "table-main" }, tbody);
+        const hiddenInput1 = React.createElement("input", { key: "hidden-input1", id: "hidden-input1", type: "hidden", value: "false" });
+        const hiddenInput2 = React.createElement("input", { key: "hidden-input2", id: "hidden-input2", type: "hidden" });
 
-        return table;
+        const divChildren = [table, hiddenInput1, hiddenInput2];
+        const div = React.createElement("div", { id: "table-div" }, divChildren);
+
+        return div;
     }
 
     render() {
